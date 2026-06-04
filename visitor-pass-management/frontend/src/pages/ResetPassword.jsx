5280a0e79ab5191c42d64e8904b041504c9c0ec8
Mohit Chaurasiya <mohit.chaurasiya0857@gmail.com>
2026-06-04 15:25:03 +0530

    reset-password page added

--- visitor-pass-management/frontend/src/pages/ResetPassword.jsx
+++ visitor-pass-management/frontend/src/pages/ResetPassword.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-function ResetPassword() {
-  return <div></div>;
-}
+const resetPassword = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-export default ResetPassword;
+    const user = await User.findOne({
+      email,
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  resetPassword,
+};
