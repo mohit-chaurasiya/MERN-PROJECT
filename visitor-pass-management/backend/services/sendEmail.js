6980a97d87e8a1919f6215843c7944a519062b94
Mohit Chaurasiya <mohit.chaurasiya0857@gmail.com>
2026-06-04 14:31:43 +0530

    Switched email service to Brevo API

--- visitor-pass-management/backend/services/sendEmail.js
+++ visitor-pass-management/backend/services/sendEmail.js
@@ -1,50 +1,61 @@
-const nodemailer = require('nodemailer')
-
-const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
-
-
-transporter.verify(function (error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("SMTP Ready");
-    }
-});
+const axios = require("axios");
 
 const sendMail = async (email, otp) => {
-    const info = await transporter.sendMail({
-        from: `"Visitor Pass Management" <${process.env.SENDER_EMAIL}>`,
-        to: email,
+  try {
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Visitor Pass Management",
+          email: process.env.SENDER_EMAIL,
+        },
+
+        to: [
+          {
+            email: email,
+          },
+        ],
+
         subject: "OTP Verification",
 
-        html: `
-        <div style="font-family:Arial,sans-serif;padding:20px">
+        htmlContent: `
+          <div style="font-family: Arial, sans-serif; padding:20px">
             <h2>Visitor Pass Management</h2>
 
-            <p>Your OTP is:</p>
+            <p>Your OTP for verification is:</p>
 
-            <h1 style="letter-spacing:5px">${otp}</h1>
+            <h1 style="letter-spacing:5px; color:#6d28d9;">
+              ${otp}
+            </h1>
 
             <p>This OTP is valid for 5 minutes.</p>
 
-            <p>This is system generated email . Please do not reply</p>
-        </div>
+            <hr>
+
+            <p style="color:gray;">
+              If you didn't request this OTP, please ignore this email.
+            </p>
+          </div>
         `,
-    });
+      },
+      {
+        headers: {
+          accept: "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+          "content-type": "application/json",
+        },
+      }
+    );
 
-    console.log("MAIL SENT:", info.messageId);
+    console.log("MAIL SENT:", response.data);
+    return true;
+  } catch (error) {
+    console.error(
+      "BREVO ERROR:",
+      error.response?.data || error.message
+    );
+    throw error;
+  }
 };
-
-
-
 
 module.exports = sendMail;
