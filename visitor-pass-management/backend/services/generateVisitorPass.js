ba5053e2b3a85ce311fdd44e4f31ce62ff2f800b
Mohit Chaurasiya <mohit.chaurasiya0857@gmail.com>
2026-06-06 09:04:26 +0530

    modify pdf bug

--- visitor-pass-management/backend/services/generateVisitorPass.js
+++ visitor-pass-management/backend/services/generateVisitorPass.js
@@ -2,17 +2,17 @@ const PDFDocument = require("pdfkit")
 const QRCode = require("qrcode")
 const path = require("path")
 
-const generateVisitorPass = async (pass)=>{
+const generateVisitorPass = async (pass) => {
 
     return new Promise(async (resolve) => {
-        
-        const doc = new PDFDocument({size: "A6", margin: 20})
+
+        const doc = new PDFDocument({ size: "A5", margin: 20 })
 
         const buffers = []
 
         doc.on("data", buffers.push.bind(buffers))
 
-        doc.on("end",()=>{
+        doc.on("end", () => {
             const pdfData = Buffer.concat(buffers)
             resolve(pdfData)
         })
@@ -24,65 +24,75 @@ const generateVisitorPass = async (pass)=>{
 
         // border
 
-        doc.rect(10,10,doc.page.width-20,doc.page.height -20).stroke()
+        doc.rect(10, 10, doc.page.width - 20, doc.page.height - 20).stroke()
 
-        doc.fontSize(16).text("Company Logo",20,20)
-        doc.fontSize(10).fillColor("gray").text("Visitor Management System",20,40)
+        doc.fontSize(16).text("Company Logo", 20, 20)
+        doc.fontSize(10).fillColor("gray").text("Visitor Management System", 20, 40)
 
         const qrImage = await QRCode.toDataURL(pass.passNumber)
-        doc.image(qrImage,doc.page.width-100,20,{width:80})
+        doc.image(qrImage, doc.page.width - 100, 20, { width: 80 })
 
         console.log(pass.visitorId.photo)
-        if(pass.visitorId.photo){
-        const photoPath = path.join(__dirname,"..",pass.visitorId.photo)
-        
-        doc.circle(50,110,30).clip()
-        doc.image(photoPath,20,80,{
-            width: 60,
-            height: 60
-        })   
-    }
+        if (pass.visitorId.photo) {
 
-     doc.moveDown(3)
+            const photoPath = path.join(
+                __dirname,
+                "..",
+                pass.visitorId.photo
+            );
 
-    // Pass Title
-    doc.fontSize(14)
-       .fillColor("#000")
-       .text("VISITOR PASS", { align: "center" })
+            doc.save();
 
-    doc.moveDown()
+            doc.circle(50, 110, 30).clip();
 
-    // Pass Number Highlight
-    doc.fontSize(12)
-       .fillColor("blue")
-       .text(`Pass No: ${pass.passNumber}`, { align: "center" })
+            doc.image(photoPath, 20, 80, {
+                width: 60,
+                height: 60
+            });
 
-    doc.moveDown(2)
+            doc.restore();
+        }
 
-    // Visitor Details
-    doc.fillColor("#000")
-    doc.fontSize(11)
+        doc.moveDown(3)
 
-    doc.text(`Visitor Name         : ${pass.visitorId.name}`)
-    doc.text(`Host Name            : ${hostName}`)
-    doc.text(`Purpose              : ${purpose}`)
-    doc.text(`Date of Appointment  : ${appointmentDate}`)
+        // Pass Title
+        doc.fontSize(14)
+            .fillColor("#000")
+            .text("VISITOR PASS", { align: "center" })
 
-    doc.moveDown()
+        doc.moveDown()
 
-    doc.text(`Valid From : ${pass.validFrom.toLocaleString()}`)
-    doc.text(`Valid Till  : ${pass.validTill.toLocaleString()}`)
+        // Pass Number Highlight
+        doc.fontSize(12)
+            .fillColor("blue")
+            .text(`Pass No: ${pass.passNumber}`, { align: "center" })
 
-    doc.moveDown(2)
+        doc.moveDown(2)
 
-    // Footer
-    doc.fontSize(9)
-       .fillColor("gray")
-       .text("Please wear this pass visibly during your visit.", {
-           align: "center"
-       })
+        // Visitor Details
+        doc.fillColor("#000")
+        doc.fontSize(11)
 
-    doc.end()
+        doc.text(`Visitor Name         : ${pass.visitorId.name}`)
+        doc.text(`Host Name            : ${hostName}`)
+        doc.text(`Purpose              : ${purpose}`)
+        doc.text(`Date of Appointment  : ${appointmentDate}`)
+
+        doc.moveDown()
+
+        doc.text(`Valid From : ${pass.validFrom.toLocaleString()}`)
+        doc.text(`Valid Till  : ${pass.validTill.toLocaleString()}`)
+
+        doc.moveDown(2)
+
+        // Footer
+        doc.fontSize(9)
+            .fillColor("gray")
+            .text("Please wear this pass visibly during your visit.", {
+                align: "center"
+            })
+
+        doc.end()
 
     })
 }
